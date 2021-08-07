ec82184ab3ea1f9fe4331e69d9dcb4bd854597ed
Crowdbotics <team@crowdbotics.com>
2021-08-07 01:02:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "damp_leaf_29593Identifier",
+  url: "https://damp_leaf_29593Identifier.botics.co",
+  api: "https://damp_leaf_29593Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
